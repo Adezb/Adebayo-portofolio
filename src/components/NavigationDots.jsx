43d7eff9f73352289e22ab852f2c0bc7704fbfdf
Adezb <temitopeceo@gmail.com>
2023-02-09 21:09:49 +0100

    Error fix

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -3,18 +3,6 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-<<<<<<< HEAD
-      {["home", "about", "contact", "work", "skills"].map((item, index) => (
-        <a
-          href={`#${item}`}
-          key={item + index}
-          className="app__navigation-dot"
-          style={active === item ? { backgroundColor: "#313bac" } : {}}
-        >
-          {active === item && <span className="app__navigation-dot--active" />}
-        </a>
-      ))}
-=======
       {["home", "about", "contact", "work", "skills", "testimonial"].map(
         (item, index) => (
           <a
@@ -27,7 +15,6 @@ const NavigationDots = ({ active }) => {
           </a>
         )
       )}
->>>>>>> 818082e1a0e3fab907f7eb0382d5706f6d7e4412
     </div>
   );
 };
